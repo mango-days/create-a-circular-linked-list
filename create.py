83b329a816b8f4b6b2a6ee13ae1b58b6135a7075
mango-days <positivejasminekaur@gmail.com>
2022-01-20 23:50:07 +0530

    first commit

---
+++ create.py
@@ -0,0 +1,49 @@
+# create a circularly linked list
+
+class Node :
+    def __init__ ( self , data ) :
+        self.data = data
+        self.next = None
+
+class CircularLinkedList :
+    def __init__ ( self ) :
+        self.head = None
+    
+    def insert ( self , data ) :
+        if self.head == None :
+            self.head = Node ( data )
+            self.head.next = self.head
+            return
+        
+        # find last element
+        temp = self.head.next
+        prev = self.head
+        flag = self.head
+        while temp != flag :
+            prev = temp
+            if temp.next != None : temp = temp.next
+            
+        # insert element
+        prev.next = Node ( data )
+        prev.next.next = flag
+        return
+    
+    def printList ( self ) :
+        if self.head == None :
+            print ( "List empty" )
+            return
+        
+        temp = self.head.next
+        prev = self.head
+        flag = self.head
+        while temp != flag :
+            print ( temp.data )
+            temp = temp.next
+        return
+            
+CllObj = CircularLinkedList ()
+
+for index in range ( 10 ) : 
+    CllObj.insert ( index )
+    
+CllObj.printList ()
