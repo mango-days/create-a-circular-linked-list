715e69282dfbd0c9988df6163db2edae2a243b76
mango-days <positivejasminekaur@gmail.com>
2022-01-21 23:15:14 +0530

    first commit

--- create.py
+++ create.py
@@ -1,11 +1,12 @@
-# create a circularly linked list
+# 2 way linked list
 
 class Node :
     def __init__ ( self , data ) :
+        self.prev = None
         self.data = data
         self.next = None
 
-class CircularLinkedList :
+class DoubleLinkedList :
     def __init__ ( self ) :
         self.head = None
     
@@ -13,37 +14,47 @@ class CircularLinkedList :
         if self.head == None :
             self.head = Node ( data )
             self.head.next = self.head
+            self.head.prev = self.head
+            print ( self.head.prev.data ,"  ", self.head.data ,"  ", self.head.next.data )
             return
         
-        # find last element
-        temp = self.head.next
-        prev = self.head
-        flag = self.head
-        while temp != flag :
-            prev = temp
-            if temp.next != None : temp = temp.next
-            
-        # insert element
-        prev.next = Node ( data )
-        prev.next.next = flag
+        temp = self.head
+        while temp :
+            if temp.next != self.head : temp = temp.next
+            else : break
+        
+        temp.next = Node ( data )
+        temp.next.next = self.head
+        temp.next.prev = temp
+        
+        self.head.prev = temp.next
+        print ( temp.next.prev.data ,"  ", temp.next.data ,"  ", temp.next.next.data )
         return
     
     def printList ( self ) :
-        if self.head == None :
-            print ( "List empty" )
-            return
+        if self.head == None : return
         
+        print ( "pre", "node" , "next" )
+
+        print ( self.head.prev.data ,"  ", self.head.data ,"  ", self.head.next.data )
         temp = self.head.next
         prev = self.head
         flag = self.head
         while temp != flag :
-            print ( temp.data )
+            print ( temp.prev.data ,"  ", temp.data ,"  ", temp.next.data )
             temp = temp.next
         return
-            
-CllObj = CircularLinkedList ()
-
-for index in range ( 10 ) : 
-    CllObj.insert ( index )
     
-CllObj.printList ()
+    
+
+Obj = DoubleLinkedList ()
+
+print ( "inserting nodes ---" )
+print ( "pre", "node" , "next" )
+for index in range (9) : Obj.insert (index+1)
+
+print ("")
+print ( "nodes inserted as ---" )
+Obj.printList()
+
+#print ( Obj.find(5) )
